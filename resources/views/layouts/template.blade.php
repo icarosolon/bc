66443c299abd52459196e2c4cb1f04e247603ba7
unknown <icaro.solon.is@gmail.com>
2020-12-11 22:38:11 -0300

    ajustes para utilizar templates

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -111,54 +111,13 @@ color: #14A378;
         <div class="d-flex justify-content-center h-100">
             <div class="card">
                 <div class="card-header">
-                    <h3>Login</h3>
+                    <h3>@yield('title')</h3>
                     <div class="d-flex justify-content-end social_icon">
                         <span><i class="fas fa-hospital-alt"></i></span>
                         <span><i class="fas fa-brain"></i></span>
                     </div>
                 </div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-                        <div class="input-group form-group form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" id="email" name="email" class="form-control" placeholder="usuário" required>   
-                        </div>
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                        <div class="input-group form-group form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-key"></i></span>
-                            </div>
-                            <input type="password" id="password" name="password" class="form-control" placeholder="senha" required>
-                        </div>
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                        <div class="row align-items-center remember">
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>Lembrar senha
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" value="Entrar" class="btn float-right login_btn">
-                        </div>
-                    </form>
-                </div>
-                
-                <div class="card-footer">
-                    <div class="d-flex justify-content-center links">
-                        Não tem uma conta?<a href="{{ route('register') }}">Cadastre-se</a>
-                    </div>
-                    <div class="d-flex justify-content-center links">
-                        <a href="{{ route('password.request') }}">Esqueceu sua senha?</a>
-                    </div>
-                </div>
+                @yield('content')
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('user', 'UserController');
 Route::resource('article', 'ArticleController');
+Route::get('/teste', function () {
+    return view('articles.index');
+});
